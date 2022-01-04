1190cf7ea48cd650b089a6d499af49c20a7a6e5f
Skyz03 <aakibdiscoo@gmail.com>
2022-01-04 23:26:32 +0545

    Comments added

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const secondHand = document.querySelector(".second-hand");
 const minsHand = document.querySelector(".min-hand");
-const hourHand = document.querySelector(".hour-hand");
+const hourHand = document.querySelector(".hour-hand");  // This one gets the DOM
 
 
 function setDate() {
 
-  const now = new Date();
-  const seconds = now.getSeconds();
-  const secondDegrees = ((seconds / 60) * 360) + 90;
-  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+  const now = new Date();  // Function to get New Date
+  const seconds = now.getSeconds();  //Fucntion to get every Seconds 
+  const secondDegrees = ((seconds / 60) * 360) + 90;  //Calculation to change it into degree
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`; //DOM select plus style transform to apply rotate every 1second
 
   const mins = now.getMinutes();
   const minsDegrees = ((mins / 60) * 360) + 90;
@@ -20,4 +20,4 @@ function setDate() {
 
 }
 
-setInterval(setDate, 1000);
+setInterval(setDate, 1000); //Function which is being called every second.
